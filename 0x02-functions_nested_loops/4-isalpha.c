86e4247b010da5b9b33810bc8d66b1e915e2712b
DYozar <79377769+DYozar@users.noreply.github.com>
2023-03-21 16:43:41 +0100

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _islower - it's the main function
+ *  _isalpha - it's the main function
  * @c: it-s a parameter
  * Return: should be 1 if c is a letter or 0 if otherwise
  */
