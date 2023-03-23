a717cb344959cfae26a4c1ecfc776c13cedf24fc
DYozar <donyozar@gmail.com>
2023-03-23 06:38:07 -0700

    advnc0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,7 +11,7 @@ void print_times_table(int n)
 {
 	int i, j;
 
-	if (n < 0 || n > 15)
+	if (n > 15 || n < 0)
 		return;
 
 	for (i = 0; i <= n; i++)
@@ -20,27 +20,8 @@ void print_times_table(int n)
 		{
 			if (j == 0)
 				_putchar('0');
-			else if (i * j < 10)
-			{
-				printf("0" + i * j);
-				printf(",   ");
-			}
-			else if (i * j < 100)
-			{
-				printf(",  ");
-				_putchar('0' + (i * j) / 10);
-				_putchar('0' + (i * j) % 10);
-			}
 			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar('0' + (i * j) / 100);
-				_putchar('0' + ((i * j) / 10) % 10);
-				_putchar('0' + (i * j) % 10);
-				if (i * j == 100)
-					_putchar(' ');
-			}
+				_printf(", %d", i * j);
 		}
 		_putchar('\n');
 	}
