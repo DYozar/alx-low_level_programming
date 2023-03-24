6866b942e28cbbdc5b1089874c00fefe7f30b287
DYozar <donyozar@gmail.com>
2023-03-24 08:26:19 -0700

    10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -16,11 +16,9 @@ void print_triangle(int size)
 		return;
 	}
 
-	int i, j;
-
 	for (i = 0; i < size; i++)
 	{
-		for (j = size - i; j > 1; j--)
+		for (j = 0; j < size - i - 1; j++)
 			_putchar(' ');
 		for (j = 0; j <= i; j++)
 			_putchar('#');
