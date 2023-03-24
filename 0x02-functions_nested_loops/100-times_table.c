e3cb6776dcb3b6a8dd27592b3b4be658691164dc
DYozar <donyozar@gmail.com>
2023-03-24 12:41:41 -0700

    advnc1

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,20 +9,39 @@
 
 void print_times_table(int n)
 {
-	int i, j;
+	int a, b, op;
 
-	if (n > 15 || n < 0)
-		return;
-
-	for (i = 0; i <= n; i++)
+	if (n >= 0 && n <= 15)
 	{
-		for (j = 0; j <= n; j++)
+		for (a = 0; a <= n; a++)
 		{
-			if (j == 0)
-				_putchar('0');
-			else
-				printf(", %d", i * j);
+			_putchar(48);
+			for (b = 1; b <= n; b++)
+			{
+				op = a * b;
+				_putchar(44);
+				_putchar(32);
+				if (op <= 9)
+				{
+					_putchar(32);
+					_putchar(32);
+					_putchar(op + 48);
+				}
+				else if (op <= 99)
+				{
+					_putchar(32);
+					_putchar((op / 10) + 48);
+					_putchar((op % 10) + 48);
+				}
+				else
+				{
+					_putchar(((op / 100) % 10) + 48);
+					_putchar(((op / 10) % 10) + 48);
+					_putchar((op % 10) + 48);
+				}
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
+
 }
