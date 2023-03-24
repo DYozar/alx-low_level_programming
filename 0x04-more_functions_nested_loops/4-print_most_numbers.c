7ae615fdafdf64c9a35e8577d1d9d2f38ed9b967
DYozar <donyozar@gmail.com>
2023-03-24 07:20:48 -0700

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * void print_most_numbers - main function
+ * print_most_numbers - main function
  * Return: should be 0
  */
 
 void print_most_numbers(void)
 {
 	int i;
+
 	for (i = 0; i < 10; i++)
 	{
 		if (i != 2 && i != 4)
