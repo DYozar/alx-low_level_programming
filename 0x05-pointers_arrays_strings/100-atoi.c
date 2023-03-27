e68c772ab6c51b16fe4d65356c6f810a7e7771a7
DYozar <79377769+DYozar@users.noreply.github.com>
2023-03-27 17:34:02 +0200

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,28 +11,26 @@ int _atoi(char *s)
     int sign = 1;
     int result = 0;
     int i = 0;
-
-    /* Skip all non-digit characters at the beginning of the string */
+/* Skip all non-digit characters at the beginning of the string */
     while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
     {
-        /* Check for sign characters */
+/* Check for sign characters */
         if (s[i] == '-')
             sign *= -1;
-
-        i++;
+         i++;
     }
 
-    /* Read all digit characters until the end of the string or a non-digit character */
+/* Read all digit characters until the end of the string or a non-digit character */
     while (s[i] != '\0' && s[i] >= '0' && s[i] <= '9')
     {
-        /* Add the digit to the result, making sure to handle overflow */
+/* Add the digit to the result, making sure to handle overflow */
         result = result * 10 + (s[i] - '0');
 
-        /* Move to the next digit */
+/* Move to the next digit */
         i++;
     }
 
-    /* Apply the sign and return the result */
+/* Apply the sign and return the result */
     return sign * result;
 }
 
