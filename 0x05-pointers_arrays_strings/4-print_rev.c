34ecb694610083689c79afdfbc444a1baae72303
DYozar <donyozar@gmail.com>
2023-03-27 07:34:18 -0700

    task4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,15 +6,16 @@
  */
 void print_rev(char *s)
 {
+	int i;
 	int len = 0;
 
-	while (s[len] != '\0')
+	for(i = 0; s[i] != '\0'; i++)
 	{
 		len++;
 	}
-	for (int i = len - 1; i >= 0; i--)
+	for (i = len - 1; i >= 0; i--)
 	{
-	_putchar(s[i]);
+		_putchar(s[i]);
 	}
 	_putchar('\n');
 }
