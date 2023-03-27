d6e3793a4c9fc3107081dfb831b4eb273e4e50b8
DYozar <donyozar@gmail.com>
2023-03-27 07:59:54 -0700

    task8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_array - Prints n elements of an array of integers, followed by a nline
+ *
+ * @a: Array of integers to be printed.
+ * @n: Number of elements of the array to be printed.
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)/* Loop through each element of the array.*/
+	{
+		printf("%d", a[i]);/*Print the current element.*/
+
+		if (i != n - 1)/* If it's not the last element, print a comma and space.*/
+		{
+			printf(", ");
+		}
+	}
+
+	printf("\n");/* Print a newline character at the end.*/
+}
+
