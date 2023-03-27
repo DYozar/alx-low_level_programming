20dbee5e7b36a7f1af64731e5e6e3245df7da417
DYozar <79377769+DYozar@users.noreply.github.com>
2023-03-27 17:20:41 +0200

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,11 +9,10 @@ int main(void)
 	int i;
 
 /* Iterate over source string and copy each character to destination */
-	if (dest == NULL) return NULL;
+	if (dest == NULL)
+		return (NULL);
 	for (i = 0; src[i] != '\0'; i++)
-	{
 	    dest[i] = src[i];
-	}
 	
 	return (dest);
 }
