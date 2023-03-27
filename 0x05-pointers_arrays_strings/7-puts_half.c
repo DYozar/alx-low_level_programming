ddf86b95b44ef3676a46ab1acfed872298b1be29
DYozar <donyozar@gmail.com>
2023-03-27 07:52:51 -0700

    task7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,21 +10,21 @@ void puts_half(char *str)
 {
 	int i, len = 0, n;
 
-    /* Calculate the length of the string */
+/* Calculate the length of the string */
 	while (str[len] != '\0')
 		len++;
 
-    /* Calculate the number of characters to print */
+/* Calculate the number of characters to print */
 	if (len % 2 == 0)
 		n = len / 2;
 	else
 		n = (len + 1) / 2;
 
-    /* Print the second half of the string */
+/* Print the second half of the string */
 	for (i = n; i < len; i++)
 		_putchar(str[i]);
 
-    /* Print a new line*/	
+/* Print a new line*/
 	_putchar('\n');
 }
 
