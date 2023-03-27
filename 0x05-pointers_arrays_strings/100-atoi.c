7ec67c7850c2ac47f66762de73c39c4d78b66354
DYozar <donyozar@gmail.com>
2023-03-27 08:43:16 -0700

    advnc0

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,36 +1,26 @@
 #include "main.h"
 
 /**
- * _atoi - Convert a string to an integer
- * @s: The string to be converted
+ * _atoi - Converts a string to an integer.
+ * @s: The string to be converted.
  *
- * Return: The integer value of the string
+ * Return: The integer value of the string.
  */
 int _atoi(char *s)
 {
-	int sign = 1;
-	int result = 0;
-	int i = 0;
-/* Skip all non-digit characters at the beginning of the string */
-	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
-	{
-/* Check for sign characters */
-	if (s[i] == '-')
-		sign *= -1;
-	i++;
-	}
+	int sign = 1, i = 0;
+	unsigned int num = 0;
 
-/* Read all digit char until the end of the string or a non-digit character */
-	while (s[i] != '\0' && s[i] >= '0' && s[i] <= '9')
+	while (s[i])
 	{
-/* Add the digit to the result, making sure to handle overflow */
-		result = result * 10 + (s[i] - '0');
-
-/* Move to the next digit */
+		if (s[i] == '-')
+			sign *= -1;
+		else if (s[i] >= '0' && s[i] <= '9')
+			num = num * 10 + (s[i] - '0');
+		else if (num > 0)
+			break;
 		i++;
 	}
-
-/* Apply the sign and return the result */
-	return sign * result;
+	return (num * sign);
 }
 
