03fa566dd7cf2e8bd06b3f163989090b390ca833
DYozar <donyozar@gmail.com>
2023-03-30 14:57:00 -0700

    taskadvnc4

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -13,31 +13,31 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int carry = 0;
-int len1 = strlen(n1);
-int len2 = strlen(n2);
-int i, j;
+	int carry = 0;
+	int len1 = strlen(n1);
+	int len2 = strlen(n2);
+	int i, j;
 
-if (len1 + 1 > size_r || len2 + 1 > size_r)
-{
-return 0;
-}
+	if (len1 + 1 > size_r || len2 + 1 > size_r)
+	{
+		return (0);
+	}
 
-for (i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0; i--, j--)
-{
-int digit1 = (i >= 0) ? n1[i] - '0' : 0;
-int digit2 = (j >= 0) ? n2[j] - '0' : 0;
-int sum = digit1 + digit2 + carry;
-carry = sum / 10;
-r[i + j + 1] = sum % 10 + '0';
-}
+	for (i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0; i--, j--)
+	{
+		int digit1 = (i >= 0) ? n1[i] - '0' : 0;
+		int digit2 = (j >= 0) ? n2[j] - '0' : 0;
+		int sum = digit1 + digit2 + carry;
+		carry = sum / 10;
+		r[i + j + 1] = sum % 10 + '0';
+	}
 
-if (carry)
-{
-r[0] = carry + '0';
-return r;
-}
+	if (carry)
+	{
+		r[0] = carry + '0';
+		return (r);
+	}
 
-return r + 1;
+	return (r + 1);
 }
 
