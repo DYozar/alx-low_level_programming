ccdd1f71086fd203984cbe7114e27ac8f00e9e00
DYozar <donyozar@gmail.com>
2023-04-10 06:21:03 -0700

    done

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - it's the main function
+ * @c: it-s a parameter
+ * Return: should be 1 if c is a letter or 0 if otherwise
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+
+	else
+		return (0);
+}
---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *  _isalpha - it's the main function
+ * @c: it-s a parameter
+ * Return: should be 1 if c is a letter or 0 if otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
