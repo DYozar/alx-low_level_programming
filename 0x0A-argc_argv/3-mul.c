8486170405d7edada9ce7b78fa31827544113ab6
DYozar <79377769+DYozar@users.noreply.github.com>
2023-04-10 17:51:02 +0200

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,9 +8,7 @@
  */
 
 int _atoi(char *s)
-
 {
-
 	int i, d, n, len, f, digit;
 
 	i = 0;
@@ -23,12 +21,9 @@ int _atoi(char *s)
 	while (s[len] != '\0')
 		len++;
 	while (i < len && f == 0)
-
 	{
-
 		if (s[i] == '-')
 			++d;
-
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
@@ -42,14 +37,11 @@ int _atoi(char *s)
 				break;
 
 			f = 0;
-
 		}
 		i++;
-
 	}
 	if (f == 0)
 		return (0);
-
 	return (n);
 }
 /**
@@ -58,7 +50,7 @@ int _atoi(char *s)
  * @argv: array of arguments
  * Return: 0 (Success), 1 (Error)
  */
-		
+
 int main(int argc, char *argv[])
 {
 	int result, num1, num2;
@@ -67,13 +59,12 @@ int main(int argc, char *argv[])
 	{
 		printf("Error\n");
 		return (1);
-	}	
+	}
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
 	result = num1 * num2;
 	printf("%d\n", result);
 
 	return (0);
-
 }
 
