b011cb40a993765906e765e442f7d4db3e11b374
DYozar <donyozar@gmail.com>
2023-05-05 06:20:49 -0700

    all

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * flip_bits - returns the number of bits you would need to flip to get from one number to another
+ * flip_bits - returns the number of bits
  * @n: first number
  * @m: second number
  *
@@ -17,6 +17,6 @@ num_bits += xor_result & 1;
 xor_result >>= 1;
 }
 
-return num_bits;
+return (num_bits);
 }
 
